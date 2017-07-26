494107182d2442f490953039769379b4429308c2
eric_tu <eric_tu@localhost.localdomain>
2017-07-26 15:14:08 +0800

    [Bug] fix path in files

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,5 @@
 <?php
 
 $input = $request->get('name','world');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF8')));
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 
