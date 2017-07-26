380c7de2f615783df99afa4a012d8925e3d1f2a9
eric_tu <eric_tu@localhost.localdomain>
2017-07-26 14:14:53 +0800

    [Feature] extra code and replace with init.php

--- bye.php
+++ bye.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+//require_once __DIR__.'/vendor/autoload.php';
+//
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//$request = Request::createFromGlobals();
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__.'/init.php';
 
-$request = Request::createFromGlobals();
+//$response = new Response('Goodbye!');
 
-$response = new Response('Goodbye!');
+$response->setContent('Goodbye!');
+
 $response->send();
