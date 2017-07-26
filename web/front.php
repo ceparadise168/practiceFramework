4c2d7a627609937094c87caecc55b2166090430b
eric_tu <eric_tu@localhost.localdomain>
2017-07-26 16:59:35 +0800

    temp

--- web/front.php
+++ web/front.php
@@ -4,8 +4,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
 
 $request  = Request::createFromGlobals();
+
+$routes = new RouteCollection();
+$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
+$routes->add('bye', new Route('/bye'));
 
 $map = array(
         '/hello' => 'hello',
