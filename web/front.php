2acfdb5d08e51b644ca9e6779d3942cf564ba698
eric_tu <eric_tu@localhost.localdomain>
2017-07-26 16:43:43 +0800

    [Bug] make code more readable

--- web/front.php
+++ web/front.php
@@ -3,25 +3,23 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\HttpFoundation\Response;
 
 $request  = Request::createFromGlobals();
-$response = new Response();
 
 $map = array(
-    '/hello' => __DIR__.'/../src/pages/hello.php',
-    '/bye' => __DIR__.'/../src/pages/bye.php',
-);
+        '/hello' => 'hello',
+        '/bye' => 'bye',
+        );
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
     ob_start();
-    include $map[$path];
-    $response->setContent(ob_get_clean());
+    extract($request->query->all(), EXTT_SKIP);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    $response = new Response(ob_get_clean());
 } else {
     $response->setStatusCode(404);
-    $response->setContent('Not Found');
 }
 
 $response->send();
