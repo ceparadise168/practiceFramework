36d304db559207f0e5e6ebb98168ed1f7136cb03
eric_tu <eric_tu@localhost.localdomain>
2017-07-27 13:44:21 +0800

    [Bug] fix front.php

--- web/front.php
+++ web/front.php
@@ -4,40 +4,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestConetxt;
-use Symfony\Component\Routing\Macher\UrlMacher;
+use Symfony\Component\Routing;
 
 $request  = Request::createFromGlobals();
-
-/*
-$routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Route('/bye'));
-
-$map = array(
-        '/hello' => 'hello',
-        '/bye' => 'bye',
-        );
-
-$path = $request->getPathInfo();
-
-if (isset($map[$path])) {
-    ob_start();
-    extract($request->query->all(), EXTT_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
-    $response = new Response(ob_get_clean());
-} else {
-    $response->setStatusCode(404);
-}
-*/
-
 $routes = include __DIR__.'/../src/app.php';
 
-$context = new Requestcontext();
+$context = new Routing\RequestContext();
 $context->fromRequest($request);
-$matcher = new UrlMacher($routes, $context);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
     extract($matcher->match($request->getPathInfo()), EXRT_SKIP);
