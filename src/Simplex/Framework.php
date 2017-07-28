11c332a51dc8aa13e2e7b680000529f8dd60344c
eric_tu <eric_tu@localhost.localdomain>
2017-07-28 14:48:37 +0800

    [Feature] add ResposneEvent.php

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 namespace Simplex;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -8,64 +8,25 @@ use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Framework
 {
     protected $matcher;
     protected $controllerResolver;
     protected $argumentResolver;
+    private $dispatcher;
 
     public function __construct(
         UrlMatcher $matcher,
         ControllerResolver $controllerResolver,
-        ArgumentResolver $argumentResolver
+        ArgumentResolver $argumentResolver,
+        EventDispatcher $dispatcher
     ) {
         $this->matcher = $matcher;
         $this->controllerResolver = $controllerResolver;
         $this->argumentResolver = $argumentResolver;
-    }
-
-    public function handle(Request $request)
-    {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-            $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
-var_dump(call_user_func_array($controller, $arguments));
-            return call_user_func_array($controller, $arguments);
-        } catch (ResourceNotFoundException $e) {
-            return new Response('Not Found', 404);
-        } catch (\Exception $e) {
-            return new Response('An error occurred', 500);
-        }
-    }
-}
-
-*/
-
-namespace Simplex;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-
-class Framework
-{
-    protected $matcher;
-    protected $controllerResolver;
-    protected $argumentResolver;
-
-    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
-    {
-        $this->matcher = $matcher;
-        $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver = $argumentResolver;
+        $this->dispatcher = $dispatcher;
     }
 
     public function handle(Request $request)
@@ -78,11 +39,14 @@ class Framework
             $controller = $this->controllerResolver->getController($request);
             $arguments = $this->argumentResolver->getArguments($request, $controller);
 
-            return call_user_func_array($controller, $arguments);
+            $response =  call_user_func_array($controller, $arguments);
         } catch (ResourceNotFoundException $e) {
-            return new Response('Not Found', 404);
+            $response =  new Response('Not Found', 404);
         } catch (\Exception $e) {
-            return new Response('An error occurred', 500);
+            $response =  new Response('An error occurred', 500);
         }
+        $this->dispatcher->dispatch('response', new ResponseEvent($response, $request));
+
+        return $resposne;
     }
 }
